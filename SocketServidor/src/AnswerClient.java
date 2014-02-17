ba41c67f2627fb5020aa68d5a4634fe9885b65f2
Ronyerison <ronyerisonbraga@gmail.com>
2014-02-17 13:58:09 -0300

    Servidor enviando para todos os clientes

--- SocketServidor/src/AnswerClient.java
+++ SocketServidor/src/AnswerClient.java
@@ -4,19 +4,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class AnswerClient implements Runnable{
-	private DataOutputStream toClient;
+	private List<DataOutputStream> toClient;
 	//private Scanner fromClient;
 	BufferedReader fromClient;
 	private InetAddress IP;
 	
-	public AnswerClient (Socket socket) throws IOException{
+	public AnswerClient (Socket socket, List<Socket> listaSockets) throws IOException{
 		//fromClient = new Scanner(socket.getInputStream());
 		fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		toClient = new DataOutputStream(socket.getOutputStream());
+//		toClient = new DataOutputStream(socket.getOutputStream());
+		toClient = new ArrayList<DataOutputStream>();
+		for (int i = 0; i < listaSockets.size(); i++) {
+			toClient.add(new DataOutputStream(listaSockets.get(i).getOutputStream()));
+		}
 		IP = socket.getInetAddress();
 	}
 	
@@ -27,7 +33,9 @@ public class AnswerClient implements Runnable{
 			while((text = fromClient.readLine()) != null){
 				System.out.println("From " + IP + ": " + text);
 				try {
-					toClient.writeBytes(text.toUpperCase() + '\n');
+					for (DataOutputStream toCliente : toClient) {
+						toCliente.writeBytes(text.toUpperCase() + '\n');
+					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
--- SocketServidor/src/TCPServer.java
+++ SocketServidor/src/TCPServer.java
@@ -1,14 +1,22 @@
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TCPServer {
+	
 	public static void main(String[] args) throws IOException{
+		List<Socket> ipsConectados = new ArrayList<Socket>();
 		ServerSocket serverSocket = new ServerSocket(5000);
+		
 		while(true){
 			Socket socket = serverSocket.accept();
-			new Thread(new AnswerClient(socket)).start();
+			ipsConectados.add(socket);
+			new Thread(new AnswerClient(socket, ipsConectados)).start();
+			
 		}
 	}
 	
