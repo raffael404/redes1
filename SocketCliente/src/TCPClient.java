9bb69dfb0abb9260e19afe4c6046f05a40fc017e
raffael404 <raffael404@gmail.com>
2014-02-17 18:31:32 -0300

    Socket Cliente adicionado
    Consegui adicionar o socket cliente no mesmo repositorio.

--- SocketCliente/src/TCPClient.java
+++ SocketCliente/src/TCPClient.java
@@ -5,17 +5,12 @@ public class TCPClient {
 	
 	public static void main(String[] args) throws Exception{
 		String sentence;
-//		while(true){
 		Socket clientSocket = new Socket("10.28.14.222", 5000);
 		new Thread(new ReceiveMessage(clientSocket)).start();
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-//		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		while((sentence = inFromUser.readLine())!=null)
 			outToServer.writeBytes(sentence + "\n");
-//		modifiedSentence = inFromServer.readLine();
-//		System.out.println("FROM SERVER: " + modifiedSentence);
 		clientSocket.close();
-//		}
 	}
 }
