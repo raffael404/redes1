94f380fce168945805dfcba12f19daf2b7b2e177
raffael404 <raffael404@gmail.com>
2014-02-20 09:57:56 -0300

    Cliente com IP certo

--- SocketCliente/src/TCPClient.java
+++ SocketCliente/src/TCPClient.java
@@ -8,14 +8,12 @@ public class TCPClient {
 	DataOutputStream outToServer;
 	
 	public TCPClient(JTextArea textArea) throws UnknownHostException, IOException {
-		clientSocket = new Socket("localhost", 5000);
+		clientSocket = new Socket("10.28.14.222", 5000);
 		new Thread(new ReceiveMessage(clientSocket, textArea)).start();
 		outToServer = new DataOutputStream(clientSocket.getOutputStream());
 	}
 	
 	public void enviaMensagem(String mensagem) throws Exception{
-		
-//		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		outToServer.writeBytes(mensagem + "\n");
 	}
 }
